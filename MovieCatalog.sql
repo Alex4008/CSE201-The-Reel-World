d395ecb0956167c840c2bd30bb728ff520a53701
Linh Nguyen <nguyen44@miamioh.edu>
2020-04-01 15:50:56 -0400

    040120 - Change sql code to MySQL

--- MovieCatalog.sql
+++ MovieCatalog.sql
@@ -1,70 +1,87 @@
-﻿DROP DATABASE IF EXISTS MovieCatalog;
-GO
-
-CREATE DATABASE MovieCatalog;
-GO
-
-USE MovieCatalog;
-GO
+-- ﻿DROP DATABASE IF EXISTS MovieCatalog;
+-- GO
+--
+-- CREATE DATABASE MovieCatalog;
+-- GO
+--
+-- USE MovieCatalog;
+-- GO
 
 CREATE TABLE Users(
-	userId			INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	userName		VARCHAR(50)		NOT NULL,
-	[password]		VARCHAR(100)	NOT NULL,
-	displayName		VARCHAR(200),
-	isDeleted		BIT				NOT NULL	DEFAULT(0),
+	userId					INT UNSIGNED        NOT NULL    AUTO_INCREMENT,
+	userName				VARCHAR(50)		    NOT NULL,
+	password				VARCHAR(100)	    NOT NULL,
+	displayName			VARCHAR(200),
+	isDeleted				TINYINT(1)				NOT NULL	DEFAULT 0,
+	PRIMARY KEY     (userId)
 );
 
 CREATE TABLE Genres(
-	genreId			INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	[description]	VARCHAR(200)	NOT NULL,
-	isDeleted		BIT				NOT NULL	DEFAULT(0)
+	genreId					INT UNSIGNED 		NOT NULL	   AUTO_INCREMENT,
+  description	    VARCHAR(200)	        NOT NULL,
+	isDeleted				TINYINT(1)				NOT NULL	DEFAULT 0,
+	PRIMARY KEY     (genreId)
 );
 
 CREATE TABLE Requests(
-	requestId			INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	userId				INT				NOT NULL	FOREIGN KEY REFERENCES Users(userId),
-	requestDate			DATETIME		NOT NULL	DEFAULT(GETDATE()),
-	requestName			VARCHAR(100),
-	[description]		VARCHAR(300)
+	requestId					INT UNSIGNED		NOT NULL	AUTO_INCREMENT,
+	userId						INT UNSIGNED		NOT NULL,
+	requestDate				DATETIME		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+	requestName				VARCHAR(100),
+	description		    VARCHAR(300),
+	PRIMARY KEY       (requestId),
+	FOREIGN KEY       (userId) REFERENCES Users(userId)
 );
 
 CREATE TABLE Actors(
-	actorId					INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	requestId				INT				NOT NULL	FOREIGN KEY REFERENCES Requests(requestId),
+	actorId					INT	UNSIGNED			NOT NULL	AUTO_INCREMENT,
+	requestId				INT	UNSIGNED			NOT NULL,
 	actorName				VARCHAR(100)	NOT NULL,
 	actorLink				VARCHAR(2000),
-	isDeleted				BIT				NOT NULL	DEFAULT(0)
+	isDeleted				TINYINT(1)				NOT NULL	DEFAULT 0,
+	PRIMARY KEY     (actorId),
+	FOREIGN KEY     (requestId) REFERENCES Requests(requestId)
 );
 
 CREATE TABLE Movies(
-	movieId					INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	requestId				INT				NOT NULL	FOREIGN KEY REFERENCES Requests(requestId),
-	title					VARCHAR(200)	NOT NULL,
-	[description]			VARCHAR(3000),
+	movieId					INT	UNSIGNED			NOT NULL	AUTO_INCREMENT,
+	requestId				INT	UNSIGNED			NOT NULL,
+	title						VARCHAR(200)	NOT NULL,
+	description			VARCHAR(3000),
 	keywords				VARCHAR(2000),
 	imdbLink				VARCHAR(3000),
-	[image]					VARBINARY(500),
-	imageAddress			VARCHAR(3000),
+	image						VARBINARY(500),
+	imageAddress		VARCHAR(3000),
 	rating					FLOAT,
-	isDeleted				BIT				NOT NULL	DEFAULT(0)
+	isDeleted				TINYINT(1)				NOT NULL	DEFAULT 0,
+	PRIMARY KEY     (movieId),
+	FOREIGN KEY     (requestId) REFERENCES Requests(requestId)
 );
 
 CREATE TABLE Comments(
-	commentId			INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	userId				INT				FOREIGN KEY REFERENCES Users(userId),
-	movieId				INT				NOT NULL	FOREIGN KEY REFERENCES	Movies(movieId),
-	commentText			VARCHAR(3000)	NOT NULL
+	commentId				INT	UNSIGNED			NOT NULL	AUTO_INCREMENT,
+	userId					INT	UNSIGNED			NOT NULL,
+	movieId					INT	UNSIGNED			NOT NULL,
+	commentText			VARCHAR(3000)	        NOT NULL    DEFAULT '',
+	PRIMARY KEY     (commentId),
+	FOREIGN KEY     (userId)      REFERENCES      Users(userId),
+	FOREIGN KEY     (movieId)     REFERENCES	  Movies(movieId)
 );
 
 CREATE TABLE ActorMovie(
-	actorMovieId		INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	actorId				INT				NOT NULL	FOREIGN KEY REFERENCES	Actors(actorId),
-	movieId				INT				NOT NULL	FOREIGN KEY REFERENCES	Movies(movieId),
+	actorMovieId		INT	UNSIGNED			NOT NULL	AUTO_INCREMENT,
+	actorId					INT	UNSIGNED			NOT NULL,
+	movieId					INT	UNSIGNED			NOT NULL,
+	PRIMARY KEY         (actorMovieId),
+	FOREIGN KEY         (actorId)    REFERENCES	Actors(actorId),
+	FOREIGN KEY         (movieId)    REFERENCES	Movies(movieId)
 );
 
 CREATE TABLE GenreMovie(
-	genreMovieId		INT				NOT NULL	IDENTITY	PRIMARY KEY,
-	genreId				INT				NOT NULL	FOREIGN KEY REFERENCES	Genres(genreId),
-	movieId				INT				NOT NULL	FOREIGN KEY REFERENCES	Movies(movieId),
+	genreMovieId		INT	UNSIGNED			NOT NULL	AUTO_INCREMENT,
+	genreId					INT	UNSIGNED			NOT NULL,
+	movieId					INT	UNSIGNED			NOT NULL,
+	PRIMARY KEY         (genreMovieId),
+	FOREIGN KEY         (genreId)         REFERENCES	Genres(genreId),
+  FOREIGN KEY         (movieId)         REFERENCES	Movies(movieId)
 );
