ee07ff732a583ffcfab92b4143ef42b99d4202a8
linh_nguyen <nguyen44@miamioh.edu>
2020-04-08 15:23:41 -0400

    040820 - Dummy data 2 insertions

---
+++ MovieCatalogDummyData2.sql
@@ -0,0 +1,36 @@
+INSERT INTO Genres (description) VALUES('Biography'), ('History'), ('Musical'), ('Family');
+
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (191, 'Rex Harrison', 'https://www.imdb.com/name/nm0001322/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (2, 'Warren Beatty', 'https://www.imdb.com/name/nm0000886/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (2, 'Faye Dunaway', 'https://www.imdb.com/name/nm0001159/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (120, 'Brad Pitt', 'https://www.imdb.com/name/nm0000093/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (120, 'Kevin Spacey', 'https://www.imdb.com/name/nm0000228/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (20, 'Bruce Willis', 'https://www.imdb.com/name/nm0000246/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (20, 'Alan Rickman', 'https://www.imdb.com/name/nm0000614/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (20, 'Bonnie Bedelia', 'https://www.imdb.com/name/nm0000889/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (37, 'F. Murray Abraham', 'https://www.imdb.com/name/nm0000719/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (37, 'Tom Hulce', 'https://www.imdb.com/name/nm0001371/');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (37, 'Elizabeth Berridge', 'https://www.imdb.com/name/nm0000931/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (67, 'Billy Crudup', 'https://www.imdb.com/name/nm0001082/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (67, 'Patrick Fugit', 'https://www.imdb.com/name/nm0297578/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (67, 'Kate Hudson', 'https://www.imdb.com/name/nm0005028/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (73, 'Christian Bale', 'https://www.imdb.com/name/nm0000288/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (73, 'Heath Ledger', 'https://www.imdb.com/name/nm0005132/?ref_=tt_ov_st_sm');
+INSERT INTO Actors (requestId, actorName, actorLink) VALUES (73, 'Aaron Eckhart', 'https://www.imdb.com/name/nm0001173/?ref_=tt_ov_st_sm');
+
+
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (191, 'My Fair Lady', 'Snobbish phonetics Professor Henry Higgins agrees to a wager that he can make flower girl Eliza Doolittle presentable in high society.', 'love, romance, musical', 'https://www.imdb.com/title/tt0058385/?ref_=nm_knf_i4', NULL, 'https://m.media-amazon.com/images/M/MV5BNGM0ZTU3NmItZmRmMy00YWNjLWEzMWItYzg3MzcwZmM5NjdiXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_UY268_CR6,0,182,268_AL_.jpg', 7.8);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (1, 'Cleopatra', 'Queen Cleopatra VII of Egypt experiences both triumph and tragedy as she attempts to resist the imperial ambitions of Rome.', 'politics, historical, Egypt', 'https://www.imdb.com/title/tt0056937/?ref_=nm_knf_i2', NULL, 'https://m.media-amazon.com/images/M/MV5BY2YyOGFlZWItY2M5OC00NzJhLWJmNDgtZTVjNzIzMDI3YjgyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX182_CR0,0,182,268_AL_.jpg', 7.0);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (2, 'Bonnie And Clyde', 'Bored waitress Bonnie Parker falls in love with an ex-con named Clyde Barrow and together they start a violent crime spree through the country, stealing cars and robbing banks.', 'romance, crime, Bonnie', 'https://www.imdb.com/title/tt0061418/', NULL, 'https://m.media-amazon.com/images/M/MV5BOTViZmMwOGEtYzc4Yy00ZGQ1LWFkZDQtMDljNGZlMjAxMjhiXkEyXkFqcGdeQXVyNzM0MTUwNTY@._V1_UX182_CR0,0,182,268_AL_.jpg', 7.8);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (120, 'Se7en', 'Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives.', 'detectives, serial killer, seven daealy sins', 'https://www.imdb.com/title/tt0114369/', NULL, 'https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg', 8.6);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (20, 'Die Hard', 'An NYPD officer tries to save his wife and several others taken hostage by German terrorists during a Christmas party at the Nakatomi Plaza in Los Angeles.', 'hostage, NYPD, terrorists', 'https://www.imdb.com/title/tt0095016/', NULL, 'https://m.media-amazon.com/images/M/MV5BZjRlNDUxZjAtOGQ4OC00OTNlLTgxNmQtYTBmMDgwZmNmNjkxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg', 8.2);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (37, 'Amadeus', 'The life, success and troubles of Wolfgang Amadeus Mozart, as told by Antonio Salieri, the contemporaneous composer who was insanely jealous of Mozart''s talent and claimed to have murdered him.', 'Mozart', 'https://www.imdb.com/title/tt0086879/', NULL, 'https://m.media-amazon.com/images/M/MV5BNWJlNzUzNGMtYTAwMS00ZjI2LWFmNWQtODcxNWUxODA5YmU1XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_UX182_CR0,0,182,268_AL_.jpg', 8.3);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (67, 'Almost Famous', 'A high-school boy is given the chance to write a story for Rolling Stone Magazine about an up-and-coming rock band as he accompanies them on their concert tour.', 'Fame, rock band, high-school', 'https://www.imdb.com/title/tt0181875/', NULL, 'https://m.media-amazon.com/images/M/MV5BMzY1ZjMwMGEtYTY1ZS00ZDllLTk0ZmUtYzA3ZTA4NmYwNGNkXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UX182_CR0,0,182,268_AL_.jpg', 7.9);
+INSERT INTO Movies (requestId, title, description, keywords, imdbLink, image, imageAddress, rating) VALUES (73, 'The Dark Knight', 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 'Batman', 'https://www.imdb.com/title/tt0468569/', NULL, 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg', 9.0);
+
+
+INSERT INTO ActorMovie(actorId, movieId) VALUES (13, 5),(17,5),(12,6),(17,6), (18, 7), (19, 7), (6, 8), (20, 8), (21, 8), (22, 9), (23, 9), (24, 9), (25, 10), (26, 10), (27, 10), (28, 11), (29, 11), (30, 11);
+INSERT INTO ActorMovie(actorId, movieId) VALUES (31, 12), (32, 12), (33, 12);
+
+INSERT INTO GenreMovie(genreId, movieId) VALUES (5,5), (24,5), (23, 5), (5,6), (21, 6), (22,6), (1, 7), (21, 7), (14, 7), (14,8), (5,8), (10, 8), (1, 9), (11, 9), (5, 10), (21, 10), (22, 10), (5, 11), (6, 11), (12, 11);
+INSERT INTO GenreMovie(genreId, movieId) VALUES (1, 12), (5, 12), (14, 12);
