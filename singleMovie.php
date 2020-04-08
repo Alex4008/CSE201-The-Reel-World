e377f20b3c5461a07dba1d7e15f339542882e427
grayad2 <grayad2@ceclnx01-19.csi.miamioh.edu>
2020-04-08 19:55:32 -0400

    Added singleMovie.php. This file is where each movies more information page will be displayed. As of right now, the connection between this and index.php has been made. But the style is very much a WIP. Anyone else should feel free to add to this.

---
+++ singleMovie.php
@@ -0,0 +1,90 @@
+<?php
+	require("db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>The Reel World</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="fav.ico">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link href='https://fonts.googleapis.com/css?family=Alegreya' rel='stylesheet'>
+	<style>
+		.left-align {
+			position: abosolute;
+			left: 5px;
+		}
+	</style>
+</head>
+<body style="font-family:Alegreya;background-color:#1e272e;">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <div class="navbar-header">
+    <a class="navbar-brand" href="index.php">THE REEL WORLD</a>
+  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <!--
+      <li class="nav-item">
+        <a class="nav-link" href="#">My Favorite Movies</a>
+      </li>
+  -->
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search...">
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button" style="background-color:#c0c3c5;color:#053560;">GO</button>
+        </span>
+      </div>
+
+      <button class="btn btn-danger" type="button" style="margin:5px;margin-left:10px;">SORT</button>
+			<div class="btn-group">
+			  <button type="button" class="btn btn-danger">FILTER</button>
+			  <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			    <span class="sr-only">Toggle Dropdown</span>
+			  </button>
+			  <div class="dropdown-menu bg-secondary text-light">
+					<div class="font-weight-bold" style="margin-left:10px;">By Genre</div>
+					<form class="px-4 py-3 rounded" id="filterOptions">
+						<!-- add php here to populate options based on the Genres table -->
+						<div class="form-group" style="margin-left:10px;">
+								<input type="checkbox" class="form-check-input" id="romance" value="romance">
+                <label for="romance">Romance </label>
+            </div>
+						<div class="form-group" style="margin-left:10px;">
+								<input type="checkbox" class="form-check-input" id="action" value="action">
+                <label for="romance">Action </label>
+            </div>
+				    <button type="submit" class="btn btn-danger align-right" style="margin:10px; float:right; margin-bottom: 5px;">OK</button>
+				  </form>
+			  </div>
+			</div>
+      <button class="btn btn-warning" type="button" style="margin:5px; ">Add A Movie</button>
+    </form>
+  </div>
+</nav>
+
+<?php
+	// Run queries here.
+?>
+
+<div style="padding:15px; margin-left: 15%; margin-right: 15%; background: #f7f5f4;">
+	<h1><?php print $_REQUEST['title']; ?></h1>
+	<p>More information here.</p>
+</div>
+
+
+</body>
+</html>
